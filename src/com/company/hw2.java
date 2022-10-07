5791246dbbd9fa590837be7aa7578536c88398e0
emilyyu <yuhuayu.emily@gmail.com>
2022-10-07 08:57:10 +0300

    .

--- src/com/company/hw2.java
+++ src/com/company/hw2.java
@@ -7,7 +7,7 @@ public class hw2 {
         int [] a = {1, 3, 8, 2, -3};
         int [] b = {6, -2, 13, 17, 2};
 
-        for (int n = 0; n < a.length; n++)
+        for (int n = 0; n < a.length && !flag; n++)
             for (int m = 0; m < b.length && !flag; m++)
                 if (a [n] == b [m])
                     flag = true;
--- src/com/company/hw3.java
+++ src/com/company/hw3.java
@@ -4,10 +4,9 @@ public class hw3 {
 
     public static void main(String[] args) {
         int [] nums = {3, 7, 4, 1};
-        for (int k = 0; k < nums.length; k++) {
+        for (int k = 0; k < nums.length; k++)
             for (int l = 0; l < nums.length; l++)
                 if (k != l)
-                    System.out.println (10 * nums [l] + nums [k]);
-        }
+                    System.out.println(10 * nums[l] + nums[k]);
     }
 }
